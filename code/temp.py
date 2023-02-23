c8a30645f04747a37eaf92be355a63a6c1c3c71f
Vedmani <vedmani.vaidya@gmail.com>
2023-02-23 15:46:30 +0530

    added temp.py

--- code/temp.py
+++ code/temp.py
@@ -1,6 +1,9 @@
 import logging
-
+def main():
+    print('printed from main')
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
     logger.info("Hello world")
+    print("hello world")
+    mai()
