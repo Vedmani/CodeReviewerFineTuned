4d46002216c7588e725ae37d27bbb29e4d9387f3
Vedmani <vedmani.vaidya@gmail.com>
2023-02-23 13:48:36 +0530

    added model pred in get_model.py

--- code/get_model.py
+++ code/get_model.py
@@ -24,6 +24,21 @@ args = add_args(parser)
 set_seed(args)
 config, model, tokenizer = build_or_load_gen_model(args)
 model_size = get_model_size(model)
-model.cuda()
+model.to("cuda")
 print(model_size)
 print("Model device:", model.device)
+model.eval()
+code_diff = """@@ -11,6 +11,8 @@\n \n         invoiceDtoCopy.setState(InvoiceState.OPEN);\n         _invoiceAggregateRepository.updateInvoiceState(invoiceCopy, InvoiceState.OPEN);\n+        _erpIntegrationService.createAndSendInvoiceEvent(invoiceCopy);\n+\n       }\n     }\n \n"""
+inputs = torch.tensor([encode_diff(tokenizer, code_diff)], dtype=torch.long).to("cuda")
+inputs_mask = inputs.ne(tokenizer.pad_id)
+preds = model.generate(inputs,
+                        attention_mask=inputs_mask,
+                        use_cache=True,
+                        num_beams=5,
+                        early_stopping=True,
+                        max_length=100,
+                        num_return_sequences=2
+                        )
+preds = list(preds.cpu().numpy())
+pred_nls = [tokenizer.decode(id[2:], skip_special_tokens=True, clean_up_tokenization_spaces=False) for id in preds]
+print(pred_nls[0])
