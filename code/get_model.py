f25fed5532d8679bb8396d46f43b14a3abbddc8f
Vedmani <vedmani.vaidya@gmail.com>
2023-02-23 13:58:50 +0530

    added preds in get_model.py

--- code/get_model.py
+++ code/get_model.py
@@ -19,6 +19,29 @@ import torch.distributed as dist
 from utils import CommentClsDataset, SimpleClsDataset
 from sklearn.metrics import f1_score, accuracy_score
 
+def encode_diff(tokenizer, diff):
+    difflines = diff.split("\n")[1:]        # remove start @@
+    difflines = [line for line in difflines if len(line.strip()) > 0]
+    map_dic = {"-": 0, "+": 1, " ": 2}
+    def f(s):
+        if s in map_dic:
+            return map_dic[s]
+        else:
+            return 2
+    labels = [f(line[0]) for line in difflines]
+    difflines = [line[1:].strip() for line in difflines]
+    inputstr = ""
+    for label, line in zip(labels, difflines):
+        if label == 1:
+            inputstr += "<add>" + line
+        elif label == 0:
+            inputstr += "<del>" + line
+        else:
+            inputstr += "<keep>" + line
+    source_ids = tokenizer.encode(inputstr, max_length=MAX_SOURCE_LENGTH, truncation=True)[1:-1]
+    source_ids = pad_assert(tokenizer, source_ids)
+    return source_ids
+
 parser = argparse.ArgumentParser()
 args = add_args(parser)
 set_seed(args)
