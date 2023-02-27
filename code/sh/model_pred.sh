7f80e571204d775992e1340707eb3899a1192f2f
Vedmani <vedmani.vaidya@gmail.com>
2023-02-27 10:56:46 +0530

    multigpu args removed

--- code/sh/model_pred.sh
+++ code/sh/model_pred.sh
@@ -3,14 +3,14 @@
 mnt_dir="/content"
 
 # You may change the following block for multiple gpu training
-MASTER_HOST=localhost && echo MASTER_HOST: ${MASTER_HOST}
+"""MASTER_HOST=localhost && echo MASTER_HOST: ${MASTER_HOST}
 MASTER_PORT=23333 && echo MASTER_PORT: ${MASTER_PORT}
 RANK=0 && echo RANK: ${RANK}
 PER_NODE_GPU=1 && echo PER_NODE_GPU: ${PER_NODE_GPU}
 WORLD_SIZE=1 && echo WORLD_SIZE: ${WORLD_SIZE}
 NODES=1 && echo NODES: ${NODES}
 NCCL_DEBUG=INFO
-
+"""
 #bash test_nltk.sh
 
 
